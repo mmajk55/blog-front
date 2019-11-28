8a396707e8b8634e97ff8c47bac54130abca3a20
mmajk55 <michalmalec55@gmail.com>
2019-11-28 15:59:45 +0100

    Create feed component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route, Switch } from 'react-router-dom';
+import Feed from './Views/Feed/Feed';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" component={Feed} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Views/Feed/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Feed = props => {
+
+    return (
+        <div className="container">
+            <div className="row">
+
+            </div>
+        </div>
+    )
+}
+
+export default Feed;
